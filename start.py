2ce7d32c1199fa4418015d3a3ac9378aac2e2251
watermelon1024 <watermelonman1024@gmail.com>
2024-04-05 00:47:58 +0800

    feat: add ffmpeg_downloader package

--- start.py
+++ start.py
@@ -6,9 +6,14 @@ import tracemalloc
 
 tracemalloc.start(25)  # Used in src/client/client.py on_ready function.
 
-if __name__ == "__main__":
+
+def main():
     from decouple import config
 
     from src.main import Bot
 
     Bot(config("log-webhook")).run(config("token"))
+
+
+if __name__ == "__main__":
+    main()
